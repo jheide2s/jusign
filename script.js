651e09443402333940d983001186b3062c660936
jheide2s <julian.heidebrecht@smail.inf.h-brs.de>
2025-01-27 19:40:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll('.main-nav a');
+import { initSlideshow } from './slideshow.js';
+import { initBlaettern } from './blaettern.js';
 
-  links.forEach(link => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = event.target.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetId);
+document.addEventListener('DOMContentLoaded', async () => {
 
-      targetSection.scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const firstLookGrid = document.querySelector("f.jpg");
-
-  const images = [
-    { src: "1.jpg", text: "Ein magischer Moment..." },
-    { src: "https://via.placeholder.com/300", text: "Ein unvergesslicher Tag." },
-    { src: "https://via.placeholder.com/300", text: "Ein besonderes Lächeln." },
-    { src: "https://via.placeholder.com/300", text: "Glück und Liebe." }
-  ];
-
-  images.forEach((item) => {
-    const card = document.createElement("div");
-    card.classList.add("image-card");
-
-    const description = document.createElement("div");
-    description.classList.add("image-description");
-    description.innerHTML = `<p>${item.text}</p>`;
-
-    const img = document.createElement("img");
-    img.src = item.src;
-    img.alt = item.text;
-
-    card.appendChild(description);
-    card.appendChild(img);
-    firstLookGrid.appendChild(card);
-  });
+  await initSlideshow();
+  await initBlaettern();
 });
